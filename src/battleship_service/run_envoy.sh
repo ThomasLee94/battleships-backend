f6c45e1f8ce3b9a068e632d9c3ddda5320ff859b
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-07 17:47:03 -0700

    [fix] path

--- src/battleship_service/run_envoy.sh
+++ src/battleship_service/run_envoy.sh
@@ -4,7 +4,7 @@
 # they would be linked with ports on kubernetes for deployment
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
-pushd "$ROOT_DIR/battleship-backend/src/battleship_service/docker/envoy" 
+pushd "$ROOT_DIR/battleship-backend/src/battleship_service/envoy" 
 docker build -t tom/envoy .
 popd
 docker run -d --network=host tom/envoy 
